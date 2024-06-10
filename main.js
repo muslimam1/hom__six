42b8c8061981b91e411ed365782742f30f121220
@muslimam1 <imuslima420@gmail.com>
2024-06-10 11:04:31 +0500

    ff

---
+++ main.js
@@ -0,0 +1,115 @@
+// 1-masala
+
+// function calculatePerimeter() {
+    // const sideLength = document.getElementById('sideLength').value;
+    // const perimeter = 4 * sideLength;
+    // document.getElementById('result').innerText = 'Perimeter (P) = ${perimeter}';
+// }
+
+
+// 2-masala
+
+// function calculateArea() {
+    // const sideLength = document.getElementById('sideLength').value;
+    // const area = sideLength * sideLength;
+    // document.getElementById('result').innerText = 'Area (S) = ${area}';
+// }
+
+
+// 3-masala
+
+// function calculateRectangle() {
+    // const sideLengthA = parseFloat(document.getElementById('sideLengthA').value);
+    // const sideLengthB = parseFloat(document.getElementById('sideLengthB').value);
+    // const area = sideLengthA * sideLengthB;
+    // const perimeter = 2 * (sideLengthA + sideLengthB);
+    // document.getElementById('result').innerText = 'Area (S) = ${area}, Perimeter (P) = ${perimeter}';
+// }
+
+
+// 4-masala
+
+// function calculateCircumference() {
+    // const diameter = parseFloat(document.getElementById('diameter').value);
+    // const pi = 3.14;
+    // const circumference = pi * diameter;
+    // document.getElementById('result').innerText = 'Circumference (L) = ${circumference}';
+// }
+
+
+// 5-masala
+
+// function calculateCube() {
+    // const sideLength = parseFloat(document.getElementById('sideLength').value);
+    // const volume = Math.pow(sideLength, 3);
+    // const surfaceArea = 6 * Math.pow(sideLength, 2);
+    // document.getElementById('result').innerText = 'Volume (V) = ${volume}, Surface Area (S) = ${surfaceArea}';
+// }
+
+
+// 6-masala
+
+// function calculateParallelepiped() {
+    // const sideLengthA = parseFloat(document.getElementById('sideLengthA').value);
+    // const sideLengthB = parseFloat(document.getElementById('sideLengthB').value);
+    // const sideLengthC = parseFloat(document.getElementById('sideLengthC').value);
+    // const volume = sideLengthA * sideLengthB * sideLengthC;
+    // const surfaceArea = 2 * (sideLengthA * sideLengthB + sideLengthB * sideLengthC + sideLengthA * sideLengthC);
+    // document.getElementById('result').innerText = 'Volume (V) = ${volume}, Surface Area (S) = ${surfaceArea}';
+// }
+
+
+// 7-masala
+
+// function calculateCircle() {
+    // const radius = parseFloat(document.getElementById('radius').value);
+    // const pi = 3.14;
+    // const circumference = 2 * pi * radius;
+    // const area = pi * Math.pow(radius, 2);
+    // document.getElementById('result').innerText = 'Circumference (L) = ${circumference}, Area (S) = ${area}';
+// }
+
+
+// 8-masala
+
+// function calculateMean() {
+    // const numberA = parseFloat(document.getElementById('numberA').value);
+    // const numberB = parseFloat(document.getElementById('numberB').value);
+    // const mean = (numberA + numberB) / 2;
+    // document.getElementById('result').innerText = 'Arithmetic Mean = ${mean}';
+// }
+
+
+// 9-masala
+
+// function calculateGeometricMean() {
+    // const numberA = parseFloat(document.getElementById('numberA').value);
+    // const numberB = parseFloat(document.getElementById('numberB').value);
+// 
+    // if (numberA < 0 || numberB < 0) {
+        // document.getElementById('result').innerText = 'Please enter non-negative numbers.';
+        // return;
+    // }
+    // const geometricMean = Math.sqrt(numberA * numberB);
+    // document.getElementById('result').innerText = 'Geometric Mean = ${geometricMean}';
+// }
+
+
+// 10-masala
+
+// function calculate() {
+    // const numberA = parseFloat(document.getElementById('numberA').value);
+    // const numberB = parseFloat(document.getElementById('numberB').value);
+// 
+    // if (numberA === 0 && numberB === 0) {
+        // document.getElementById('result').innerText = 'Please enter at least one non-zero number.';
+        // return;
+    // }
+// 
+    // const sum = numberA + numberB;
+    // const product = numberA * numberB;
+    // const squareA = Math.pow(numberA, 2);
+    // const squareB = Math.pow(numberB, 2);
+// 
+    // document.getElementById('result').innerHTML = 'Sum: ${sum} Product: ${product}  Square of a: ${squareA} Square of b: ${squareB}';
+// }
